0bdb778fce50859ed824837402def73ff2651e94
Omar <omar.assed@gmail.com>
2025-04-29 09:54:27 -0600

    bug fix

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState} from 'react'
-import products from "../products";
+// import products from "../products";
 import Rating from '../components/Rating'
 import { Link } from "react-router-dom";
 import {
