e5c72154faba5f2f33f1cc5b5db31ec6cf82dc6f
Omar <omar.assed@gmail.com>
2025-05-05 17:40:54 -0600

    Added loading spinner and UI error message

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,6 +2,7 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
 
@@ -12,7 +13,7 @@ const HomeScreen = () => {
       {isLoading ? (
         <><Loader/></>
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
       ) : (
         <>
           <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,7 +3,8 @@ import Rating from "../components/Rating";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
-
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 const ProductScreen = () => {
   const { id: productId } = useParams();
 
@@ -19,9 +20,9 @@ const ProductScreen = () => {
         Go Back
       </Link>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <h2><Loader/></h2>
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
       ) : (
         <Row>
           <Col md={5}>
