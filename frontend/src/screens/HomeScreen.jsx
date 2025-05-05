f4882d78692cb0bc511b163adf438638c4a75bed
Omar <omar.assed@gmail.com>
2025-05-05 17:30:08 -0600

    Fix loader filename casing

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,7 @@
 import { Row, Col } from "react-bootstrap";
-
 import Product from "../components/Product";
-
 import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Loader from '../components/Loader'
 
 const HomeScreen = () => {
 
@@ -11,7 +10,7 @@ const HomeScreen = () => {
   return (
     <div>
       {isLoading ? (
-        <>Loading...</>
+        <><Loader/></>
       ) : error ? (
         <div>{error?.data?.message || error.error}</div>
       ) : (
