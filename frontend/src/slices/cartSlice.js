7d964a7a99ab54a4874734826b17317c6563ea47
Omar <omar.assed@gmail.com>
2025-05-05 18:33:23 -0600

    implemented add to cart functionality

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+
+const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2)
+}
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
+      //Shipping policy: if order is more than 100 dollars, shipping is free, else it is 10 dollars
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+      //Setting tax rate to 15%
+      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
+      state.totalPrice = (
+        Number(state.itemsPrice) + 
+        Number(state.shippingPrice) + 
+        Number(state.taxPrice)
+      )
+      localStorage.setItem('cart', JSON.stringify(state))
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
